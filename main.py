0319edbbc4889c150b8ad0bbc99f4d917371f696
Julio M <julio-maca>
2021-02-05 20:35:23 -0600

    Primer_comit

---
+++ main.py
@@ -0,0 +1,40 @@
+		clients='Pablo,Ricardo,'
+def create_client(client_name):
+    global clients
+    if client_name not in clients:  
+        clients+=client_name
+        _add_comma()
+    else:
+        print('Client already is in the client\' list')
+
+
+def list_clients():
+    global clients
+    print(clients)
+
+
+def _add_comma():
+    global clients
+    clients+=',' 
+
+def _print_welcome():
+    print('Welcome to alex ventas XD')
+    print('+'*50)
+    print('what would you like to do today?')
+    print('[C]reate client')
+    print('[D]elete client ')
+
+if __name__=='__main__':
+    _print_welcome()
+    command=input()
+    if command== 'C':
+        client_name=input('What is the client name? ')
+        create_client(client_name)
+        list_clients()
+    elif command== 'D':
+        pass
+    else:
+        print('Invalid command')
+
+
+    
